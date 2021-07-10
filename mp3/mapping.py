bf9d68ff137bc9a922aa00747202c76703a1d1aa
Ts Custer <ts.custer@t-online.de>
2021-07-10 10:37:19 +0200

    Rename frame_class_to_key_mapping to _frame_class_to_key_mapping

--- mp3/mapping.py
+++ mp3/mapping.py
@@ -5,7 +5,7 @@ from mutagen.id3 import TALB, TPE1, COMM, TCOM, TYER, TDRC, TCON, TIT2, TRCK
 
 from audio_tag_data import AudioTagKey
 
-frame_class_to_key_mapping = {  # 'APIC': AudioTagPicture is loaded, too, but in another way
+_frame_class_to_key_mapping = {  # 'APIC': AudioTagPicture is loaded, too, but in another way
     TALB: AudioTagKey.album,
     TPE1: AudioTagKey.artist,
     COMM: AudioTagKey.comment,
@@ -19,11 +19,11 @@ frame_class_to_key_mapping = {  # 'APIC': AudioTagPicture is loaded, too, but in
     TRCK: AudioTagKey.track_number,
 }
 
-key_to_frame_class_mapping = {v: k for k, v in frame_class_to_key_mapping.items()}
+key_to_frame_class_mapping = {v: k for k, v in _frame_class_to_key_mapping.items()}
 # Mp3TagWriter writes ID3v2.4 tags, and frame id TDRC is right for ID3v2.4:
 key_to_frame_class_mapping[AudioTagKey.year] = TDRC
 
-frame_id_to_key_mapping: Dict[str, AudioTagKey] = {k.__name__: v for k, v in frame_class_to_key_mapping.items()}
+frame_id_to_key_mapping: Dict[str, AudioTagKey] = {k.__name__: v for k, v in _frame_class_to_key_mapping.items()}
 
 key_to_frame_id_mapping: Dict[AudioTagKey, str] = {v: k for k, v in frame_id_to_key_mapping.items()}
 
